e046a9fd057e356709973933f96928e0b5e8903d
Juseung Kim <95juseung@gmail.com>
2023-06-01 11:41:34 +0900

    Output to movie data html, add cards background color in css

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+    const data = await getData();
+    posting(data);
+});
 
-})
+async function getData() {
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2M2E2MTIwODQ3MGU2NmIxYzQ3NzhmNTc3MDU2YzIzZSIsInN1YiI6IjY0NzMwMTBjYmUyZDQ5MDBmOTk0MDEzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Mf7DH--mdOf5roTRGplgCqjABQrKJcgHdqMJoeLHysU'
+        }
+    };
 
-const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2M2E2MTIwODQ3MGU2NmIxYzQ3NzhmNTc3MDU2YzIzZSIsInN1YiI6IjY0NzMwMTBjYmUyZDQ5MDBmOTk0MDEzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Mf7DH--mdOf5roTRGplgCqjABQrKJcgHdqMJoeLHysU'
-    }
+    const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+    const data = await response.json();
+    const value = data['results'];
+    return value;
 };
 
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+function posting(value) {
+    const cardsBox = document.querySelector('.cardsBox');
+    cardsBox.innerHTML = '';
+    value.forEach((a) => {
+        let originalTitle = a['original_title']
+        let overView = a['overview']
+        let voteAverage = a['vote_average']
+        let posterPath = a['poster_path']
+        posterPath = 'https://image.tmdb.org/t/p/w300' + posterPath;
+        tempHTML = `<div class="cardsBox">
+                        <div class="cards">
+                            <img src="${posterPath}">
+                            <div class="details">
+                                <h3>${originalTitle}</h3>
+                                <p>${overView}</p>
+                                <p>Rating : ${voteAverage}</p>
+                            </div>
+                        </div>
+                    </div>`;
+        cardsBox.innerHTML += tempHTML;
+    })
+}

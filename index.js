711ddd9833c0378d48be948c9f4e0b5f611fc768
Juseung Kim <95juseung@gmail.com>
2023-06-02 04:25:08 +0900

    Add movie search functionality

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     posting(get);
 });
 
+let movie = []
+
 async function getData() {
     const options = {
         method: 'GET',
@@ -14,19 +16,20 @@ async function getData() {
 
     const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
     const data = await response.json();
-    const source= data['results'];
+    const source = data['results'];
+    movie.push(source)
     return source;
 };
 
-function posting(source) {
+function posting(movies) {
     const cardsBox = document.querySelector('.cardsBox');
     cardsBox.innerHTML = '';
-    source.forEach((a) => {
-        let originalTitle = a['original_title']
-        let overView = a['overview']
-        let voteAverage = a['vote_average']
-        let posterPath = a['poster_path']
-        let id = a['id']
+    movies.forEach((a) => {
+        let originalTitle = a.original_title
+        let overView = a.overview
+        let voteAverage = a.vote_average
+        let posterPath = a.poster_path
+        let id = a.id
         posterPath = 'https://image.tmdb.org/t/p/w300' + posterPath;
         tempHTML = `<div class="cards" onclick="alert('영화 id: ${id}')">
                         <img src="${posterPath}">
@@ -37,5 +40,39 @@ function posting(source) {
                         </div>
                     </div>`
         cardsBox.innerHTML += tempHTML;
+    })
+}
+
+const searchBtn = document.querySelector('#searchBtn');
+const searchInput = document.querySelector('#searchInput');
+searchBtn.addEventListener('click', searchMovie);
+searchInput.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        searchMovie();
+    }
+})
+
+function searchMovie() {
+    const searchInput = document.querySelector('#searchInput').value;
+    const cardsBox = document.querySelector('.cardsBox');
+    cardsBox.innerHTML = '';
+    movie[0].forEach((data) => {
+        if (data.original_title.toLowerCase().includes(searchInput.toLowerCase())) {
+            let originalTitle = data.original_title
+            let overView = data.overview
+            let voteAverage = data.vote_average
+            let posterPath = data.poster_path
+            let id = data.id
+            posterPath = 'https://image.tmdb.org/t/p/w300' + posterPath;
+            tempHTML = `<div class="cards" onclick="alert('영화 id: ${id}')">
+                            <img src="${posterPath}">
+                            <div class="details">
+                                <h3>${originalTitle}</h3>
+                                <p>${overView}</p>
+                                <p>Rating : ${voteAverage}</p>
+                            </div>
+                        </div>`
+            cardsBox.innerHTML += tempHTML;
+        }
     })
 }
